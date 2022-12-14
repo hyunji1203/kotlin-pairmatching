928d570a025651d8352fcc2e14eef16423fca9c2
hyunji1203 <hj27720@gmail.com>
2022-12-14 21:32:20 +0900

    initial commit

---
+++ src/test/java/code/CodeTest.kt
@@ -0,0 +1,20 @@
+package code
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class CodeTest {
+    /*@Test
+    fun parameterTest() {
+        // given
+        val codeChecker: CodeChecker = CodeChecker.rules()
+            .limitParameters(2)
+            .build()
+
+        // when
+        val check: Boolean = codeChecker.checkParameterCount("pairmatching", true)
+
+        // then
+        Assertions.assertThat(check).`as`(codeChecker.getMessage()).isEqualTo(true)
+    }*/
+}
